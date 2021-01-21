699b0ea6cf24da05ba2c61197b3981d1966d7192
ABuksinski <ABuksinski@qa.com>
2021-01-21 16:25:39 +0000

    update

--- src/exercise/Results.java
+++ src/exercise/Results.java
@@ -3,28 +3,26 @@ package exercise;
 public class Results {
 
 	public static void main(String[] args) {
-
-		int p = 84;
-		System.out.println(p);
-		int c = 150;
-		System.out.println(c);
-		int b = 150;
-		System.out.println(b);
-		int t = p + b + c;
-		System.out.println(t);
-		int per = (t * 100) / 450;
-		System.out.println(per);
-		if (per < 60) {
-			System.out.println("fail overall");
-		} else if (((p * 100) / 150) < 60) {
-			System.out.println("fail physics");
-		} else if (((c * 100) / 150) < 60) {
-			System.out.println("fail chemistry");
-		} else if (((b * 100) / 150) < 60) {
-			System.out.println("fail biology");
-		} else {
-			System.out.println("Pass, Well Done");
-		}
+		int phys = 100;
+		int chem = 100;
+		int bio = 100;
+		System.out.println(displayResults(phys, chem, bio));
+		System.out.println(percentageResults(phys, chem, bio));
 	}
 
+	public static String displayResults(int phys, int chem, int bio) {
+		String results = "Scores\r\n";
+		results += "Biology: " + bio + "/150\r\n";
+		results += "Chemistry: " + chem + "/150\r\n";
+		results += "Physics: " + phys + "/150\r\n";
+		results += "Total: " + (bio + chem + phys) + "/450\r\n";
+		return results;
+	}
+	public static double percentageResults(int phys, int chem, int bio) {
+		double percentage = ( 100 * ( bio + chem + phys ) ) / 450;
+		return percentage;
+		
+		
+		
+	}
 }
