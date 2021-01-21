91bbc110865cc45ef3407290d453851fd9bd80b8
ABuksinski <ABuksinski@qa.com>
2021-01-21 17:14:32 +0000

    Flowchart works!

--- src/exercise/flowchart.java
+++ src/exercise/flowchart.java
@@ -1,17 +1,28 @@
 package exercise;
 
 public class flowchart {
+	private static boolean notBool;
+
 	public static void main(String[] args) {
 		int num1 = 5;
 		int num2 = 10;
-		boolean notBool = true;
+		setNotBool(false);
 		
-		if (notBool = true) {
+		if (setNotBool(true)) {
 			int add = num1 + num2;
 			System.out.println(add);
 		} else {
 			int multiply = num1 * num2;
 			System.out.println(multiply);
 		}
+	}
+
+	public static boolean isNotBool() {
+		return notBool;
+	}
+
+	public static boolean setNotBool(boolean notBool) {
+		flowchart.notBool = notBool;
+		return notBool;
 	}
 }
