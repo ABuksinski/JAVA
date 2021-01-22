4a90b7e3e9e5e87f9c4ae86652d8c76b8bcc9d1b
ABuksinski <ABuksinski@qa.com>
2021-01-22 11:18:49 +0000

    Blackjack works and I did it with no help so that's a win

--- src/exercise/BlackJack.java
+++ src/exercise/BlackJack.java
@@ -1,5 +1,24 @@
 package exercise;
 
 public class BlackJack {
+	public static void main(String[] args) {
+		int num1 = 22;
+		int num2 = 22;
+		System.out.println(determineNumber(num1, num2));
+	}
 
+	public static double determineNumber(int num1, int num2) {
+
+		if (num1 <= 21 & num1 >= num2) {
+			return num1;
+		} else if (num2 <= 21 & num2 >= num1) {
+			return num2;
+		} else if (num2 > 21 & num1 <= 21) {
+			return num1;
+		} else if (num1 > 21 & num2 <= 21) {
+			return num2;
+		} else {
+			return 0;
+		}
+	}
 }
